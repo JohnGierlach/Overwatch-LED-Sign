6c167722c5d5831bf1510af2e946b2129c58ce5b
JohnGierlach <jgierlach17@gmail.com>
2023-06-04 10:45:29 -0400

    Added switch statement for color modes

--- cpp/ow_logo_main/ow_logo_main.ino
+++ cpp/ow_logo_main/ow_logo_main.ino
@@ -5,16 +5,23 @@
 #define G2Pin 10
 #define B2Pin 11
 
+const char modeSelect;
+const char classic = '0';
+const char blue = '1';
+const char green = '2';
+const char red = '3';
+const char yellow = '4';
+const char rgb = '5'; 
 //RGB1 Pins are for the top part of Logo
 //RGB2 Pins are for the two legs of the logo
 
-// TODO : Add IR IO Pins and Setup calls
+// TODO : Add Bluetooth IO Pins and Setup calls
 // TODO : Add In RGB Modes for Sign
-// TODO : Add switch case statement to switch between color modes
 
 // Set up for IOs
 void setup() {
-
+  Serial.begin(115200);
+  Serial.println("Bluetooth is now avaliable!");
   pinMode(R1Pin, OUTPUT);
   pinMode(G1Pin, OUTPUT);
   pinMode(B1Pin, OUTPUT);
@@ -26,5 +33,22 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  switch(modeSelect){
+      
+    case classic:
+    break;
+    case blue:
+    break;
+    case green:
+    break;
+    case red:
+    break;
+    case yellow:
+    break;
+    case rgb:
+    break;
+    // Classic
+    default:
+    break;
+  }
 }
